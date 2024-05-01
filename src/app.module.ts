5133e9b301e2985d4c64743a9e3585ddd6809f18
JWC <dnckd18@nate.com>
2024-05-02 03:59:07 +0900

    fix: add TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeormConfig), BoardsModule], //
   controllers: [AppController],
   providers: [AppService],
 })
